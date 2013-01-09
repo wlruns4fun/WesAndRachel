310f7f85aacd21e0974c064cfe40ea6f64697c60
Wes Lewis <wlewis01@gmail.com>
2013-01-09 16:04:33 -0500

    [Services: Create Spring MVC Project]
    Initial project files

---
+++ WebContent/WEB-INF/jsp/WelcomeView.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Welcome</title>
+</head>
+<body>
+Hello world!
+</body>
+</html>
---
+++ src/main/java/com/wesandrachel/controllers/FoosballController.java
@@ -0,0 +1,15 @@
+package com.wesandrachel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FoosballController {
+
+	@RequestMapping(method=RequestMethod.GET, value={"/", "/test"})
+	public ModelAndView getWelcomeView() {
+		return new ModelAndView("WelcomeView");
+	}
+}
